6cee0282e66c725f70a0fe67006036cd9b9ab16a
Ziyoxon <mirziyoxonm@gmail.com>
2025-01-17 10:25:31 +0500

    Product_Rating_swagger_write1_ziyoxon

--- src/product_rating/product_rating.controller.ts
+++ src/product_rating/product_rating.controller.ts
@@ -2,32 +2,47 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductRatingService } from './product_rating.service';
 import { CreateProductRatingDto } from './dto/create-product_rating.dto';
 import { UpdateProductRatingDto } from './dto/update-product_rating.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Product_Rating')
 @Controller('product-rating')
 export class ProductRatingController {
   constructor(private readonly productRatingService: ProductRatingService) {}
 
   @Post('create')
+  @ApiOperation({ summary: 'Creating product_rating' })
+  @ApiResponse({ status: 201, description: 'Product_rating create' })
   create(@Body() createProductRatingDto: CreateProductRatingDto) {
     return this.productRatingService.create(createProductRatingDto);
   }
 
   @Get('get')
+  @ApiOperation({ summary: 'Get all product_rating' })
+  @ApiResponse({ status: 201, description: 'Get All product_rating ' })
   findAll() {
     return this.productRatingService.findAll();
   }
 
   @Get('get/:id')
+  @ApiOperation({ summary: 'Get find product_rating' })
+  @ApiResponse({ status: 201, description: 'Get Find Product_Comment ' })
   findOne(@Param('id') id: string) {
     return this.productRatingService.findOne(+id);
   }
 
   @Patch('update/:id')
-  update(@Param('id') id: string, @Body() updateProductRatingDto: UpdateProductRatingDto) {
+  @ApiOperation({ summary: 'Update product_rating ' })
+  @ApiResponse({ status: 201, description: 'Update product_rating' })
+  update(
+    @Param('id') id: string,
+    @Body() updateProductRatingDto: UpdateProductRatingDto,
+  ) {
     return this.productRatingService.update(+id, updateProductRatingDto);
   }
 
   @Delete('delete/:id')
+  @ApiOperation({ summary: 'Delete product_rating' })
+  @ApiResponse({ status: 201, description: 'Delete Product_Rating' })
   remove(@Param('id') id: string) {
     return this.productRatingService.remove(+id);
   }
